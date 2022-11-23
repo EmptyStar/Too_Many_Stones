270dd937911e0431daf3389fbd2b8d530be4e616
TheOnlyJoeEnderman <joeenderman1@gmail.com>
2022-11-23 03:12:06 +0000

    add walls

---
+++ wall.lua
@@ -0,0 +1,87 @@
+-- Greenstone
+walls.register("too_many_stones:greenstone_wall", "Greentone Wall", "tms_greenstone.png",
+		"too_many_stones:greenstone", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:greenstone_brick_wall", "Greentone Brick Wall", "tms_greenstone_brick.png",
+		"too_many_stones:greenstone_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:greenstone_cobble_wall", "Cobbled Greentone Wall", "tms_greenstone_cobble.png",
+		"too_many_stones:greenstone_cobble", default.node_sound_stone_defaults())
+-- Bluestone
+walls.register("too_many_stones:bluestone_wall", "Bluestone Wall", "tms_bluestone.png",
+		"too_many_stones:bluestone", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:bluestone_brick_wall", "Bluestone Brick Wall", "tms_bluestone_brick.png",
+		"too_many_stones:bluestone_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:bluestone_cobble_wall", "Cobbled Bluestone Wall", "tms_bluestone_cobble.png",
+		"too_many_stones:bluestone_cobble", default.node_sound_stone_defaults())
+-- Calcite
+walls.register("too_many_stones:calcite_wall", "Calcite Wall", "tms_calcite.png",
+		"too_many_stones:calcite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:calcite_brick_wall", "Calcite Brick Wall", "tms_calcite_brick.png",
+		"too_many_stones:calcite_brick", default.node_sound_stone_defaults())
+-- Carnotite
+walls.register("too_many_stones:carnotite_wall", "carnotite Wall", "tms_carnotite.png",
+		"too_many_stones:carnotite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:carnotite_brick_wall", "carnotite Brick Wall", "tms_carnotite_brick.png",
+		"too_many_stones:carnotite_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:carnotite_cobble_wall", "Cobbled carnotite Wall", "tms_carnotite_cobble.png",
+		"too_many_stones:carnotite_cobble", default.node_sound_stone_defaults())
+-- Scoria
+walls.register("too_many_stones:scoria_wall", "Scoria Wall", "tms_scoria.png",
+		"too_many_stones:scoria", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:scoria_brick_wall", "Scoria Brick Wall", "tms_scoria_brick.png",
+		"too_many_stones:scoria_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:scoria_cobble_wall", "Cobbled Scoria Wall", "tms_scoria_cobble.png",
+		"too_many_stones:scoria_cobble", default.node_sound_stone_defaults())
+-- Kyanite
+walls.register("too_many_stones:kyanite_wall", "Kyanite Wall", "tms_kyanite.png",
+		"too_many_stones:kyanite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:kyanite_brick_wall", "Kyanite Brick Wall", "tms_kyanite_brick.png",
+		"too_many_stones:kyanite_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:kyanite_cobble_wall", "Cobbled Kyanite Wall", "tms_kyanite_cobble.png",
+		"too_many_stones:kyanite_cobble", default.node_sound_stone_defaults())
+-- Sugilite
+walls.register("too_many_stones:sugilite_wall", "Sugilite Wall", "tms_sugilite.png",
+		"too_many_stones:sugilite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:sugilite_brick_wall", "Sugilite Brick Wall", "tms_sugilite_brick.png",
+		"too_many_stones:sugilite_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:sugilite_cobble_wall", "Cobbled Sugilite Wall", "tms_sugilite_cobble.png",
+		"too_many_stones:sugilite_cobble", default.node_sound_stone_defaults())
+-- Ilvaite
+walls.register("too_many_stones:ilvaite_wall", "Ilvaite Wall", "tms_ilvaite.png",
+		"too_many_stones:ilvaite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:ilvaite_brick_wall", "Ilvaite Brick Wall", "tms_ilvaite_brick.png",
+		"too_many_stones:ilvaite_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:ilvaite_cobble_wall", "Cobbled Ilvaite Wall", "tms_ilvaite_cobble.png",
+		"too_many_stones:ilvaite_cobble", default.node_sound_stone_defaults())
+-- Amazonite
+walls.register("too_many_stones:amazonite_wall", "Amazonite Wall", "tms_amazonite.png",
+		"too_many_stones:amazonite", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:amazonite_brick_wall", "Amazonite Brick Wall", "tms_amazonite_brick.png",
+		"too_many_stones:amazonite_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:amazonite_cobble_wall", "Cobbled Amazonite Wall", "tms_amazonite_cobble.png",
+		"too_many_stones:amazonite_cobble", default.node_sound_stone_defaults())
+-- Marble
+walls.register("too_many_stones:marble_wall", "Marble Wall", "tms_marble.png",
+		"too_many_stones:marble", default.node_sound_stone_defaults())
+		
+walls.register("too_many_stones:marble_brick_wall", "Marble Brick Wall", "tms_marble_brick.png",
+		"too_many_stones:marble_brick", default.node_sound_stone_defaults())
+
+walls.register("too_many_stones:marble_cobble_wall", "Cobbled Marble Wall", "tms_marble_cobble.png",
+		"too_many_stones:marble_cobble", default.node_sound_stone_defaults())
