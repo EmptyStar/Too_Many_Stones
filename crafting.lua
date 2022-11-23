2f565d27eaf47a641f3404cd7980926f7f317ba0
TheOnlyJoeEnderman <joeenderman1@gmail.com>
2022-11-23 02:55:46 +0000

    add crafting recipes

---
+++ crafting.lua
@@ -0,0 +1,230 @@
+-- mods/too_many_stones/crafting.lua
+
+-- Greenstone
+minetest.register_craft({
+	output = "too_many_stones:greenstone_brick 4",
+	recipe = {
+		{"too_many_stones:greenstone", "too_many_stones:greenstone"},
+		{"too_many_stones:greenstone", "too_many_stones:greenstone"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:greenstone_block 9",
+	recipe = {
+		{"too_many_stones:greenstone", "too_many_stones:greenstone", "too_many_stones:greenstone"},
+		{"too_many_stones:greenstone", "too_many_stones:greenstone", "too_many_stones:greenstone"},
+		{"too_many_stones:greenstone", "too_many_stones:greenstone", "too_many_stones:greenstone"},
+	}
+})
+-- Bluestone
+minetest.register_craft({
+	output = "too_many_stones:bluestone_brick 4",
+	recipe = {
+		{"too_many_stones:bluestone", "too_many_stones:bluestone"},
+		{"too_many_stones:bluestone", "too_many_stones:bluestone"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:bluestone_block 9",
+	recipe = {
+		{"too_many_stones:bluestone", "too_many_stones:bluestone", "too_many_stones:bluestone"},
+		{"too_many_stones:bluestone", "too_many_stones:bluestone", "too_many_stones:bluestone"},
+		{"too_many_stones:bluestone", "too_many_stones:bluestone", "too_many_stones:bluestone"},
+	}
+})
+-- Calcite
+minetest.register_craft({
+	output = "too_many_stones:calcite_brick 4",
+	recipe = {
+		{"too_many_stones:calcite", "too_many_stones:calcite"},
+		{"too_many_stones:calcite", "too_many_stones:calcite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:calcite_block 9",
+	recipe = {
+		{"too_many_stones:calcite", "too_many_stones:calcite", "too_many_stones:calcite"},
+		{"too_many_stones:calcite", "too_many_stones:calcite", "too_many_stones:calcite"},
+		{"too_many_stones:calcite", "too_many_stones:calcite", "too_many_stones:calcite"},
+	}
+})
+-- Carnotite
+minetest.register_craft({
+	output = "too_many_stones:carnotite_brick 4",
+	recipe = {
+		{"too_many_stones:carnotite", "too_many_stones:carnotite"},
+		{"too_many_stones:carnotite", "too_many_stones:carnotite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:carnotite_block 9",
+	recipe = {
+		{"too_many_stones:carnotite", "too_many_stones:carnotite", "too_many_stones:carnotite"},
+		{"too_many_stones:carnotite", "too_many_stones:carnotite", "too_many_stones:carnotite"},
+		{"too_many_stones:carnotite", "too_many_stones:carnotite", "too_many_stones:carnotite"},
+	}
+})
+-- Scoria
+minetest.register_craft({
+	output = "too_many_stones:scoria_brick 4",
+	recipe = {
+		{"too_many_stones:scoria", "too_many_stones:scoria"},
+		{"too_many_stones:scoria", "too_many_stones:scoria"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:scoria_block 9",
+	recipe = {
+		{"too_many_stones:scoria", "too_many_stones:scoria", "too_many_stones:scoria"},
+		{"too_many_stones:scoria", "too_many_stones:scoria", "too_many_stones:scoria"},
+		{"too_many_stones:scoria", "too_many_stones:scoria", "too_many_stones:scoria"},
+	}
+})
+-- Kyanite
+minetest.register_craft({
+	output = "too_many_stones:kyanite_brick 4",
+	recipe = {
+		{"too_many_stones:kyanite", "too_many_stones:kyanite"},
+		{"too_many_stones:kyanite", "too_many_stones:kyanite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:kyanite_block 9",
+	recipe = {
+		{"too_many_stones:kyanite", "too_many_stones:kyanite", "too_many_stones:kyanite"},
+		{"too_many_stones:kyanite", "too_many_stones:kyanite", "too_many_stones:kyanite"},
+		{"too_many_stones:kyanite", "too_many_stones:kyanite", "too_many_stones:kyanite"},
+	}
+})
+-- Sugilite
+minetest.register_craft({
+	output = "too_many_stones:sugilite_brick 4",
+	recipe = {
+		{"too_many_stones:sugilite", "too_many_stones:sugilite"},
+		{"too_many_stones:sugilite", "too_many_stones:sugilite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:sugilite_block 9",
+	recipe = {
+		{"too_many_stones:sugilite", "too_many_stones:sugilite", "too_many_stones:sugilite"},
+		{"too_many_stones:sugilite", "too_many_stones:sugilite", "too_many_stones:sugilite"},
+		{"too_many_stones:sugilite", "too_many_stones:sugilite", "too_many_stones:sugilite"},
+	}
+})
+-- Ilvaite
+minetest.register_craft({
+	output = "too_many_stones:ilvaite_brick 4",
+	recipe = {
+		{"too_many_stones:ilvaite", "too_many_stones:ilvaite"},
+		{"too_many_stones:ilvaite", "too_many_stones:ilvaite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:ilvaite_block 9",
+	recipe = {
+		{"too_many_stones:ilvaite", "too_many_stones:ilvaite", "too_many_stones:ilvaite"},
+		{"too_many_stones:ilvaite", "too_many_stones:ilvaite", "too_many_stones:ilvaite"},
+		{"too_many_stones:ilvaite", "too_many_stones:ilvaite", "too_many_stones:ilvaite"},
+	}
+})
+-- Amazonite
+minetest.register_craft({
+	output = "too_many_stones:amazonite_brick 4",
+	recipe = {
+		{"too_many_stones:amazonite", "too_many_stones:amazonite"},
+		{"too_many_stones:amazonite", "too_many_stones:amazonite"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:amazonite_block 9",
+	recipe = {
+		{"too_many_stones:amazonite", "too_many_stones:amazonite", "too_many_stones:amazonite"},
+		{"too_many_stones:amazonite", "too_many_stones:amazonite", "too_many_stones:amazonite"},
+		{"too_many_stones:amazonite", "too_many_stones:amazonite", "too_many_stones:amazonite"},
+	}
+})
+-- Marble
+minetest.register_craft({
+	output = "too_many_stones:marble_brick 4",
+	recipe = {
+		{"too_many_stones:marble", "too_many_stones:marble"},
+		{"too_many_stones:marble", "too_many_stones:marble"},
+	}
+})
+
+minetest.register_craft({
+	output = "too_many_stones:marble_block 9",
+	recipe = {
+		{"too_many_stones:marble", "too_many_stones:marble", "too_many_stones:marble"},
+		{"too_many_stones:marble", "too_many_stones:marble", "too_many_stones:marble"},
+		{"too_many_stones:marble", "too_many_stones:marble", "too_many_stones:marble"},
+	}
+})
+
+--
+-- Cooking recipes
+--
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:greenstone",
+	recipe = "too_many_stones:greenstone_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:bluestone",
+	recipe = "too_many_stones:bluestone_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:carnotite",
+	recipe = "too_many_stones:carnotite_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:scoria",
+	recipe = "too_many_stones:scoria_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:kyanite",
+	recipe = "too_many_stones:kyanite_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:sugilite",
+	recipe = "too_many_stones:sugilite_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:ilvaite",
+	recipe = "too_many_stones:ilvaite_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:amazonite",
+	recipe = "too_many_stones:amazonite_cobble",
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "too_many_stones:marble",
+	recipe = "too_many_stones:marble_cobble",
+})
