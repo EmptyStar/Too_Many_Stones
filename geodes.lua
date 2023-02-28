b3bbcbde9b4a23fc496260c418d55945c1c0ce55
TheOnlyJoeEnderman <joeenderman1@gmail.com>
2023-02-28 22:51:20 +0000

    Upload files to ''

--- geodes.lua
+++ geodes.lua
@@ -1,29 +1,33 @@
 if minetest.get_modpath("geodes") ~= nil then
 
-geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:amazonite", "too_many_stones:amazonite")
+geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:amazonite", "too_many_stones:amazonite_crystal")
 
-geodes.register_geode("too_many_stones:gabbro", "too_many_stones:calcite", "too_many_stones:amethyst", "too_many_stones:amethyst")
+geodes.register_geode("too_many_stones:gabbro", "too_many_stones:calcite", "too_many_stones:amethyst", "too_many_stones:amethyst_crystal")
 
-geodes.register_geode("too_many_stones:slate", "too_many_stones:calcite", "too_many_stones:citrine", "too_many_stones:citrine")
+geodes.register_geode("too_many_stones:slate", "too_many_stones:calcite", "too_many_stones:citrine", "too_many_stones:citrine_crystal")
 
-geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:crocoite", "too_many_stones:crocoite")
+geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:crocoite", "too_many_stones:crocoite_crystal")
 
-geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:eudialite", "too_many_stones:eudialite")
+geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:eudialite", "too_many_stones:eudialite_crystal")
 
-geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:kyanite", "too_many_stones:kyanite")
+geodes.register_geode("too_many_stones:mudstone", "too_many_stones:calcite", "too_many_stones:kyanite", "too_many_stones:kyanite_crystal")
 
-geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:rose_quartz", "too_many_stones:rose_quartz")
+geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:rose_quartz", "too_many_stones:rose_quartz_crystal")
 
-geodes.register_geode("too_many_stones:serpentine", "too_many_stones:calcite", "too_many_stones:smokey_quartz", "too_many_stones:smokey_quartz")
+geodes.register_geode("too_many_stones:serpentine", "too_many_stones:calcite", "too_many_stones:smokey_quartz", "too_many_stones:smokey_quartz_crystal")
 
-geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:vivianite", "too_many_stones:vivianite")
+geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:vivianite", "too_many_stones:vivianite_crystal")
 
 geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:opal", "too_many_stones:opal")
 
-geodes.register_geode("too_many_stones:serpentine", "too_many_stones:calcite", "too_many_stones:heliodor", "too_many_stones:heliodor")
+geodes.register_geode("too_many_stones:serpentine", "too_many_stones:calcite", "too_many_stones:heliodor", "too_many_stones:heliodor_crystal")
 
 geodes.register_geode("too_many_stones:travertine", "too_many_stones:calcite", "too_many_stones:black_opal", "too_many_stones:black_opal")
 
-geodes.register_geode("too_many_stones:pumice", "too_many_stones:calcite", "too_many_stones:celestine", "too_many_stones:celestine")
+geodes.register_geode("too_many_stones:pumice", "too_many_stones:calcite", "too_many_stones:celestine", "too_many_stones:celestine_crystal")
+
+geodes.register_geode("too_many_stones:pumice", "too_many_stones:calcite", "too_many_stones:quartz", "too_many_stones:quartz_crystal")
+
+geodes.register_geode("too_many_stones:limestone_white", "too_many_stones:calcite", "too_many_stones:prasiolite", "too_many_stones:prasiolite_crystal")
 
 end
